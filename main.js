5b6a6a803cc589637edbf27753e6fe662d7c94ce
Jacob Rathbun <jrathofbun@gmail.com>
2023-06-19 20:47:45 -0400

    Complete Lab

--- main.js
+++ main.js
@@ -4,7 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//event listener for clicking heart 
+for (let click of document.getElementsByClassName("like-glyph")) {
+  click.addEventListener("click", likePost);
+}
 
+//like post function that is called from event listener
+function likePost(event) {
+  const heart = event.target;
+  //if user clicks an empty heart it "calls server" and activates full heart
+  if (heart.textContent === EMPTY_HEART) {
+    mimicServerCall()
+      .then(() => {
+        heart.textContent = FULL_HEART;
+        heart.classList.add("activated-heart");
+      })
+      //if server call returns an error, error message appears
+      .catch((error) => {
+        document.getElementById("modal-message").textContent = error;
+        document.getElementById("modal").classList.remove("hidden");
+        setTimeout(() => {
+          document.getElementById("modal").classList.add("hidden");
+        }, 3000);
+      });
+  //if user clicks on full heart it switches to an empty heart to remove like    
+  } else {
+    
+    heart.textContent = EMPTY_HEART;
+    heart.classList.remove("activated-heart");
+  }
+}
 
 
 //------------------------------------------------------------------------------
